b02ccd8b398fe2b3b18f2ca69fc3ed9134238aea
Minhaj <minhajuddin.khan@tenpearls.com>
2018-04-18 13:24:43 +0500

    passing db context on request level

--- main.go
+++ main.go
@@ -1,22 +1,61 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/julienschmidt/httprouter"
+	"database/sql"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+	"github.com/urfave/negroni"
 )
 
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprint(w, "Welcome!\n")
+type contextKey string
+
+func (c contextKey) String() string {
+	return "mypackage context key " + string(c)
+}
+
+var (
+	contextKeyAuthtoken = contextKey("auth-token")
+	contextKeyAnother   = contextKey("another")
+)
+
+func negroLoggerMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	connStr := "user=pansy-user dbname=pansy-go password=s3cr3tp4ssw0rd sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.WithValue(r.Context(), contextKeyAuthtoken, db)
+	r = r.WithContext(ctx)
+	next.ServeHTTP(rw, r)
 }
 
 func main() {
 
-	router := httprouter.New()
-	router.GET("/", Index)
-	log.Fatal(http.ListenAndServe(":8080", router))
+	mux := mux.NewRouter()
+	mux.HandleFunc("/", sayHello)
+	n := negroni.Classic()
+	n.UseFunc(negroLoggerMiddleware)
+	n.UseHandler(mux)
 
-	fmt.Println("initialized!")
+	svr := http.Server{
+		Addr:         "0.0.0.0:8080",
+		Handler:      n,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+	svr.ListenAndServe()
+
+}
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	postgres := r.Context().Value(contextKeyAuthtoken)
+	fmt.Println(postgres)
+	fmt.Fprintf(w, "Hello World!")
 }
