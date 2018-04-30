627e6ee00a3a81cc3e191189533ec353d637e71b
Minhaj <minhajuddin.khan@tenpearls.com>
2018-04-30 15:34:24 +0500

    refactor :rocket:

--- cmd/gopansy/main.go
+++ cmd/gopansy/main.go
@@ -4,9 +4,15 @@ import (
 	"log"
 	"os"
 
-	m "github.com/minhajuddinkhan/gopansy/db"
-	s "github.com/minhajuddinkhan/gopansy/server"
+	conf "github.com/minhajuddinkhan/gopansy/config"
+	db "github.com/minhajuddinkhan/gopansy/db"
+	"github.com/minhajuddinkhan/gopansy/helpers"
+	"github.com/minhajuddinkhan/gopansy/middlewares"
+	"github.com/minhajuddinkhan/gopansy/router"
+	"github.com/minhajuddinkhan/gopansy/server"
+	"github.com/tkanos/gonfig"
 	"github.com/urfave/cli"
+	"github.com/urfave/negroni"
 )
 
 const (
@@ -16,32 +22,84 @@ const (
 	serve    = "serve"
 )
 
+func upRouter() *negroni.Negroni {
+	n := negroni.Classic()
+	n.UseFunc(middlewares.DecodeJWT)
+	n.UseFunc(middlewares.SetDbCtx)
+	n.UseHandler(router.Initiate())
+	return n
+
+}
+
+func upConfig(c *conf.Configuration) {
+
+	env := os.Getenv("ENV")
+	if len(env) == 0 {
+		env = "dev"
+	}
+	path := conf.GetEnvPath(env)
+	err := gonfig.GetConf("../../config/"+path, c)
+
+	if err != nil {
+		helpers.HandleBootstrapError(err)
+	}
+
+}
+
 func main() {
 
 	app := cli.NewApp()
 	app.Name = "Pansy"
 	app.Usage = "make an explosive entrance"
 	app.Action = func(c *cli.Context) error {
+
+		var configuration conf.Configuration
+		var s server.Server
+		upConfig(&configuration)
+
 		arg := c.Args().First()
+		dbPath := "../../db/migrations"
 
 		switch arg {
 
 		case migrate:
-			m.Migrate()
-			return nil
-		case seedup:
-			m.SeederUp()
-			return nil
-		case seeddown:
-			m.SeederDown()
+
+			migrator := db.Migrator{
+				Conf: configuration,
+			}
+			migrator.Migrate(dbPath)
 			return nil
 
+		case seedup:
+
+			seeder := db.Seeder{
+				Conf: configuration,
+			}
+			return seeder.Seed()
+
+		case seeddown:
+
+			seeder := db.Seeder{
+				Conf: configuration,
+			}
+			return seeder.Seed()
+
 		case serve:
-			s.Serve()
+
+			s = server.Server{
+				Conf:   configuration,
+				Router: upRouter(),
+			}
+			s.Start()
 			return nil
 
 		default:
-			s.Serve()
+			upConfig(&configuration)
+			s = server.Server{
+				Conf:   configuration,
+				Router: upRouter(),
+			}
+			s.Start()
 			return nil
 
 		}
